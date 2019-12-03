c6d32e7404acc83f849b08372661d1be040c78d8
Daniel Wilder <dauncy@gmail.com>
2019-12-03 10:17:16 -0800

    final

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -2,11 +2,11 @@ class PlantsController < ApplicationController
 
     def index
         @plants = Plant.all
-        byebug
+        
     end 
 
     def show
         @plant = Plant.find(params[:id])
-        byebug
+       
     end 
 end
