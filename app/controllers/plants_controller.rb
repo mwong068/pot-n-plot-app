5c69a5e8cf71d1134e4e251391d630acfd699abc
Daniel Wilder <dauncy@gmail.com>
2019-12-03 10:08:54 -0800

    views

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,12 @@
 class PlantsController < ApplicationController
+
+    def index
+        @plants = Plant.all
+        byebug
+    end 
+
+    def show
+        @plant = Plant.find(params[:id])
+        byebug
+    end 
 end
