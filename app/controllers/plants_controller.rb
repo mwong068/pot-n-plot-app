5a66612967e97ec5f028dba5242ffda6b852f7a9
mwong068 <mwong068@gmail.com>
2019-12-04 16:42:39 -0800

    search bar

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,11 +1,19 @@
 class PlantsController < ApplicationController
+    
     def index
-        @plants = Plant.all 
+        if params[:query]
+            @search = params[:query].downcase
+            @plants = Plant.all.where("Lower(common_name) LIKE ?", "%#{@search}%")
+            
+        else
+            @plants = Plant.all
+        end
     end
 
     def show
         @plant = Plant.find(params[:id])
         @garden_plant = GardenPlant.new
     end
+    
     
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -3,4 +3,12 @@ class Plant < ApplicationRecord
     has_many :garden_plants
     has_many :gardens, through: :garden_plants
     
+    # def self.search(search)
+    #     if search
+    #         self.where(name: search)
+    #     else
+    #         Plant.all
+    #     end
+    # end
+
 end
