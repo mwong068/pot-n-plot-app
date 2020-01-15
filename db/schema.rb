1eac4774edc59c349dcf7c05a86e1595f72d0c1e
Daniel Wilder <dauncy@gmail.com>
2020-01-15 10:38:14 -0800

    postgres baby

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_12_06_015717) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
     t.integer "friend_id"
