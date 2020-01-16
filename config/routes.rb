41da412197f49cfb2fd2a41325b71d016a9ae917
Daniel Wilder <dauncy@gmail.com>
2020-01-15 21:07:29 -0800

    time to seed

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   post '/signup' => 'users#create'
   get '/login' => 'sessions#new'
   post '/sessions' => 'sessions#create'
-  get '/logout' => 'sessions#destroy' 
+  get '/' => 'sessions#destroy' 
   delete '/sessions' => 'sessions#destroy'
   # get '/gardens/:id?water', to: 'gardens#show', as: "water"
   # post '/gardens/:id?water', to: 'gardens#show', as: "water"
